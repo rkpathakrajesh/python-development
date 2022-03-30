39bcf86b56731ed72419739b8685c82680781df5
RKPathak <67626157+rkpathakrajesh@users.noreply.github.com>
2022-03-30 17:32:56 +0530

    Add files via upload

--- searching.py
+++ searching.py
@@ -1,12 +1,14 @@
 shoping_list =["Milk", "pasta", "eggs", "spam","bread","rice"]
 
-item_to_find = "albatross"
+item_to_find = "Albatross"
 found_at = None
 
 for index in range(len(shoping_list)):
     if shoping_list[index] == item_to_find:
         found_at=index
         break
+# if item_to_find in shoping_list:
+#     found_at = shoping_list.index[item_to_find]
 
 if found_at is not None:
     print("Item found at poxition: {}".format(found_at))
